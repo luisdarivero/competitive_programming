304d52cb0e892587da31726f9d8489c96259104d
Luis Daniel <luisdarivero.s@gmail.com>
2021-06-20 12:19:56 -0500

    first commit

---
+++ src/main/java/com/competitive/programming/ProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.competitive.programming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/competitive/programming/ProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.competitive.programming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
